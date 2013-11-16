32ca564d064aac4e29f8a4c28752c7d02c376d71
Harrison Ding <harrisond2003@gmail.com>
2013-11-16 15:27:01 -0800

    new things

---
+++ MVFBLAApp/src/com/mvfbla/app2014/Global.java
@@ -0,0 +1,14 @@
+package com.mvfbla.app2014;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public class Global {
+	public static final float WIDTH = 700;
+	public static final float HEIGHT = 700;
+	
+	public static App2014Main game;
+	
+	public static AssetManager assets;
+	public static Skin skin;
+}
