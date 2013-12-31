5a54b4f303093f6e47561b7935110ddaed66a498
mingyli34 <mingyli34@gmail.com>
2013-12-31 14:47:48 -0800

    made extend class Submission. wow. many modular. such OOP. very inherit

--- Forums/src/com/ptype/forums/classes/Comment.java
+++ Forums/src/com/ptype/forums/classes/Comment.java
@@ -1,36 +1,12 @@
 package com.ptype.forums.classes;
-
-public class Comment {
-	private String comment;
-	private int numLikes;
+//DELETE THIS FILE
+public class Comment extends Submission{
+//	private String comment;
+//	private int numLikes;
 	
 	public Comment() {
 		
 	}
 	
-	public Comment(String comment){
-		this.comment = comment;
-	}
 	
-	public String getComment() {
-		return comment;
-	}
-	
-	public int getLikes() {
-		return numLikes;
-	}
-	
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-	
-	public void incrementLikes() {
-		numLikes++;
-	}
-	
-	public void decrementLikes() {
-		if(numLikes > 0) {
-			numLikes--;
-		}
-	}
 }
