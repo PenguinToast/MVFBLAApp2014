80d49688c87f9992ba19d26a460e001280757feb
mingyli34 <mingyli34@gmail.com>
2013-12-31 14:48:28 -0800

    made extend class Submission. wow. many OOP. very inherit.

--- Forums/src/com/ptype/forums/classes/Question.java
+++ Forums/src/com/ptype/forums/classes/Question.java
@@ -1,77 +1,27 @@
 package com.ptype.forums.classes;
 
-import java.util.ArrayList;
-
 import com.ptype.forums.ForumActivity;
-
 import android.text.format.Time;
 
-public class Question implements Comparable<Question>{
-	private String question;
-	private ArrayList<Comment> comments;
-	private int numLikes, numViews, numComments, sortBy;
-	private Time timePosted;
+public class Question extends Submission implements Comparable<Question>{
+	private int sortBy;//int to determine how to sort. by time submitted, rating, or number of views
 	
-	public Question() {
-		comments = new ArrayList<Comment>(0);
-		numLikes = 0;
-		numViews = 0;
-		numComments = comments.size();
-		timePosted = new Time();
-		timePosted.setToNow();
+	public Question(){
+		this("");
 	}
 	
-	public Question(String question) {
-		this();
-		this.question = question;
+	public Question(String text){
+		super(text);
 	}
 	
-	public int getNumComments(){
-		return numComments;
-	}
-	
-	public String getQuestion() {
-		return question;
-	}
-	
-	public void setQuestion(String newQuestion) {
-		question = newQuestion;
-	}
-	
-	public void addComment(String comment) {
-		comments.add(new Comment(comment));
-	}
-	
-	public void addComment(Comment comment) {
-		comments.add(comment);
-	}
-	
-	public void addComment(ArrayList<Comment> comments) {
-		this.comments.addAll(comments);
-	}
-	
-	public ArrayList<Comment> getComments() {
-		return comments;
-	}
-	
-	public int getNumLikes() {
-		return numLikes;
-	}
-	
-	public int getNumViews(){
-		return numViews;
-	}
-	
-	public Time getTimePosted(){
-		return this.timePosted;
-	}
-	
+	//set what method to sort submissions by. constants are in ForumActivity
 	public void setSortBy(int x){
 		sortBy = x;
 	}
 	
+	//allows questions to be sorted by time posted
 	@Override
-	public int compareTo(Question q){//allows questions to be sorted by time posted
+	public int compareTo(Question q){
 //		return Time.compare(timePosted, q.timePosted);
 		switch(sortBy){
 		case ForumActivity.SORT_TIME:
@@ -88,27 +38,4 @@ public class Question implements Comparable<Question>{
 		}
 	}
 	
-	public void incrementLikes() {
-		numLikes++;
-	}
-	
-	public void decrementLikes() {
-		numLikes--;
-	}
-	
-	public void incrementComments(){
-		numComments++;
-	}
-	
-	public void decrementComments(){
-		numComments--;
-	}
-	
-	public void incrementViews(){
-		numViews++;
-	}
-	
-	public void decrementViews(){
-		numViews--;
-	}
 }
