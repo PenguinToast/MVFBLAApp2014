cddf24bb79515d13379430e5d6a67418d3c751c4
mingyli34 <mingyli34@gmail.com>
2013-12-31 16:10:02 -0800

    documentation

--- Forums/src/com/ptype/forums/classes/Reply.java
+++ Forums/src/com/ptype/forums/classes/Reply.java
@@ -1,9 +1,13 @@
+/* This class is a type of submission. It represents 
+ * the replies to questions and comments.
+ */
+
 package com.ptype.forums.classes;
 
 public class Reply extends Submission{
 	
 	public Reply(){
-		this("");
+		this("");//calls overloaded constructor. empty string
 	}
 	
 	public Reply(String comment){
