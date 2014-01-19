4fd6b8d9595ec3a8a95985f86207d097ee38cd4a
Harrison Ding <harrisond2003@gmail.com>
2014-01-18 17:44:52 -0800

    yay button change!!!!

---
+++ MADMVFBLA2014/src/com/mvfbla/madmvfbla2014/MainActivity.java
@@ -0,0 +1,46 @@
+package com.mvfbla.madmvfbla2014;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.View;
+
+import com.mvfbla.madmvfbla2014.fragments.MainFragment;
+
+public class MainActivity extends FragmentActivity {
+
+	private MainFragment mainFragment;
+
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+
+	    if (savedInstanceState == null) {
+	        // Add the fragment on initial activity setup
+	        mainFragment = new MainFragment();
+	        getSupportFragmentManager()
+	        .beginTransaction()
+	        .add(android.R.id.content, mainFragment)
+	        .commit();
+	    } else {
+	        // Or set the fragment from restored state info
+	        mainFragment = (MainFragment) getSupportFragmentManager()
+	        .findFragmentById(android.R.id.content);
+	    }
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void toForumScreen (View view) {
+		Intent intent = new Intent(this, ForumActivity.class);
+		this.startActivity(intent);
+	}
+
+}
