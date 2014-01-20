e6a148dcecccef187313061df98b9a23da700f24
Harrison Ding <harrisond2003@gmail.com>
2014-01-20 15:01:18 -0800

    new stuff with the user

--- MADMVFBLA2014/src/com/mvfbla/madmvfbla2014/classes/User.java
+++ MADMVFBLA2014/src/com/mvfbla/madmvfbla2014/classes/User.java
@@ -1,5 +1,36 @@
 package com.mvfbla.madmvfbla2014.classes;
 
-public class User {
 
+public class User{
+
+	private static int POINTS = 0;
+	private static String ID = "";
+	private static String USERNAME = "" ;
+	
+	public static void setUsername(String firstName, String lastName) {
+		USERNAME = firstName + " " + lastName;
+	}
+	
+	public static String getUsername() {
+		return USERNAME;
+	}
+	public static void setPoints(int numPoints) {
+		POINTS = numPoints;
+	}
+
+	public static int getPoints() {
+		return POINTS;
+	}
+	
+	public static void incrementPoints() {
+		POINTS = getPoints()+1;
+	}
+
+	public static void setId(String id) {
+		ID = id;
+	}
+
+	public static String getId() {
+		return ID;
+	}
 }
