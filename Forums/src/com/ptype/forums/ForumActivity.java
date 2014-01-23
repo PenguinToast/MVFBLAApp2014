619483c0f7da9ef0ac729767daec89b0fba531d8
mingyli34 <mingyli34@gmail.com>
2014-01-22 21:13:54 -0800

    title changes upon selection of drawer items
    Signed-off-by: mingyli34 <mingyli34@gmail.com>

--- Forums/src/com/ptype/forums/ForumActivity.java
+++ Forums/src/com/ptype/forums/ForumActivity.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,6 +26,9 @@ public class ForumActivity extends Activity {
 	private String[] drawerTitles;
 	private DrawerLayout mDrawerLayout;
 	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
 
 	// Custom Adapter that allows the list to expand when a group is clicked on
 	private ExpandableListAdapter expAdapter;
@@ -54,6 +58,27 @@ public class ForumActivity extends Activity {
                 R.layout.drawer_list_item, drawerTitles));
         // Set the list's click listener
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
 
 	
 		questions = setStandardGroup();
@@ -114,29 +139,29 @@ public class ForumActivity extends Activity {
 	
 	/** Swaps fragments in the main content view */
 	private void selectItem(int position) {
-	    // Create a new fragment and specify the planet to show based on position
+//	     Create a new fragment and specify the planet to show based on position
 //	    Fragment fragment = new PlanetFragment();
 //	    Bundle args = new Bundle();
 //	    args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
 //	    fragment.setArguments(args);
-//
-//	    // Insert the fragment by replacing any existing fragment
+
+	    // Insert the fragment by replacing any existing fragment
 //	    FragmentManager fragmentManager = getFragmentManager();
 //	    fragmentManager.beginTransaction()
 //	                   .replace(R.id.content_frame, fragment)
 //	                   .commit();
-//
-//	    // Highlight the selected item, update the title, and close the drawer
-//	    mDrawerList.setItemChecked(position, true);
-//	    setTitle(mPlanetTitles[position]);
-//	    mDrawerLayout.closeDrawer(mDrawerList);
+
+	    // Highlight the selected item, update the title, and close the drawer
+	    mDrawerList.setItemChecked(position, true);
+	    setTitle(drawerTitles[position]);
+	    mDrawerLayout.closeDrawer(mDrawerList);
 	}
 
-//	@Override
-//	public void setTitle(CharSequence title) {
-//	    mTitle = title;
-//	    getActionBar().setTitle(mTitle);
-//	}
+	@Override
+	public void setTitle(CharSequence title) {
+	    mTitle = title;
+	    getActionBar().setTitle(mTitle);
+	}
 
 	
 	// Creates the options menu
