5a20e874cec86465d92ea3059ac852df0bd39c93
Harrison Ding <harrisond2003@gmail.com>
2014-01-29 21:31:54 -0800

    Signed-off-by: Harrison Ding <harrisond2003@gmail.com>

--- MADMVFBLA2014/src/com/mvfbla/madmvfbla2014/MainActivity.java
+++ MADMVFBLA2014/src/com/mvfbla/madmvfbla2014/MainActivity.java
@@ -40,12 +40,7 @@ public class MainActivity extends FragmentActivity {
 
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+
 	
 	//switch intent to Forums
 	public void toForumScreen (View view) {
