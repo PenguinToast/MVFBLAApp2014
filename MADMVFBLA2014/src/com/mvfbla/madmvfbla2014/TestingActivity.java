fd89192cf397497c1115320bca03b2e96ddaa71f
William Sheu <java.user.00001@gmail.com>
2014-01-29 20:44:15 -0800

    Fixed question parsing

--- MADMVFBLA2014/src/com/mvfbla/madmvfbla2014/TestingActivity.java
+++ MADMVFBLA2014/src/com/mvfbla/madmvfbla2014/TestingActivity.java
@@ -1,5 +1,5 @@
-/* This class represents the activity
- * in which the user may take practice tests for his or her competition.
+/*
+ * This class represents the activity in which the user may take practice tests for his or her competition.
  */
 
 package com.mvfbla.madmvfbla2014;
@@ -27,12 +27,12 @@ public class TestingActivity extends DrawerActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_test);
 		reset();
-			
-		super.initNavDrawer();//initialize navigation drawer from DrawerActivity
+
+		super.initNavDrawer();// initialize navigation drawer from DrawerActivity
 	}
 
 	private ArrayList<String> getQuestion() {
-		try {//read the questions, which are retrieve from the server
+		try {// read the questions, which are retrieve from the server
 			String[] files = getAssets().list("questions");
 			int chosenFile = (int) (Math.random() * files.length);
 			InputStream is = getAssets().open("questions/" + files[chosenFile]);
@@ -40,41 +40,35 @@ public class TestingActivity extends DrawerActivity {
 			String line = null;
 			ArrayList<ArrayList<String>> questions = new ArrayList<ArrayList<String>>();
 			ArrayList<String> current = null;
-			int ansCount = 0;
-			boolean inQuestion = false;
+			int ansCount = 5;
 			while ((line = bis.readLine()) != null) {
-				if (line.equals("")) {
+				if (line.trim().equals("")) {
 					continue;
 				}
-				if (inQuestion) {
-					ansCount++;
-					if (ansCount <= 4) {
-						current.add(line);
-					} else {
-						ansCount = 0;
-						inQuestion = false;
-					}
+				ansCount++;
+				if (ansCount <= 4) {
+					current.add(line);
 				} else {
-					if(current != null) {
+					ansCount = 0;
+					if (current != null) {
 						questions.add(current);
 					}
 					current = new ArrayList<String>(5);
 					current.add(line);
-					inQuestion = true;
 				}
 			}
 			questions.add(current);
-			return questions.get((int)(Math.random() * questions.size()));
+			return questions.get((int) (Math.random() * questions.size()));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
-	//refreshes the questions
+
+	// refreshes the questions
 	public void reset() {
 		ArrayList<String> question = getQuestion();
-		
+
 		answer = new String(question.get(1));
 
 		Button[] ans = new Button[4];
@@ -83,8 +77,8 @@ public class TestingActivity extends DrawerActivity {
 		ans[2] = (Button) findViewById(R.id.Answer3);
 		ans[3] = (Button) findViewById(R.id.Answer4);
 		// To set the answers, just do ans1.setText("answer here") for each of them
-		for(int i = 0; i < 4; i++) {
-			int chosen = (int)(Math.random() * (question.size() - 1));
+		for (int i = 0; i < 4; i++) {
+			int chosen = (int) (Math.random() * (question.size() - 1));
 			ans[i].setText(question.remove(chosen + 1));
 		}
 
@@ -97,23 +91,25 @@ public class TestingActivity extends DrawerActivity {
 		this.answer = a;
 	}
 
-	//called if user answers a question correctly
+	// called if user answers a question correctly
 	public void correct() {
 		Network.sendObject(new NetAddPoints(1));
-		Toast.makeText(this, "Yay! You got it right! You earned 1 point!",Toast.LENGTH_SHORT).show();;
+		Toast.makeText(this, "Yay! You got it right! You earned 1 point!", Toast.LENGTH_SHORT).show();
+		;
 
 		reset();
 	}
 
-	//called if user answers a question incorrectly
+	// called if user answers a question incorrectly
 	public void incorrect() {
 		// Show dialog for fail
-		Toast.makeText(this, "Sorry! You got it wrong! The answer was " + answer,Toast.LENGTH_SHORT).show();;
+		Toast.makeText(this, "Sorry! You got it wrong! The answer was " + answer, Toast.LENGTH_SHORT).show();
+		;
 		reset();
 	}
 
 	/* These methods check if answers are correctly chosen. */
-	
+
 	public void checkAnswer1(View view) {
 		Button ans = (Button) view;
 		if (ans.getText().equals(answer)) {
